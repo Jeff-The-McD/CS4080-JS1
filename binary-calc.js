0d7f07a773de4b88dc10f4431e3d6ed1ee2e597c
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-04-05 23:00:56 -0700

    added and or funcs

--- binary-calc.js
+++ binary-calc.js
@@ -1,24 +1,29 @@
-function add() {
+function add(opr1, opr2) {
+    return (opr1+opr2);
 
 }
 
-function sub() {
+function sub(opr1, opr2) {
+    return (opr1-opr2);
 
 }
 
-function mult() {
+function mult(opr1, opr2) {
+    return (opr1*opr2);
 
 }
 
-function sub() {
+function div(opr1, opr2) {
+    return (opr1/opr2);
+}
+
+function and(opr1, opr2) {
+    return(opr1&&opr2);
 
 }
 
-function and() {
-
-}
-
-function or() {
+function or(opr1, opr2) {
+    return(opr1||opr2);
 
 }
 
@@ -40,20 +45,52 @@ while (!done) {
     console.log("3.Subtraction");
     console.log("4.Multiplication");
     console.log("5.Division");
+    console.log("6.And");
+    console.log("7.Or");
 
     while (wrong) {
-        var operation = parseInt(prompt('Choose 1-5:'));
-        if (operation >= 1 || operation < 6 || operation == 0)
+        var operation = parseInt(prompt('Choose 1-7:'));
+        if (operation >=0 || operation <8)
             wrong = false;
     }
 
+    // convert operation only requires 1 operand
     if (operation == 1) {
         var operand1 = parseInt(prompt('Enter the number to convert:'));
         var result = conv(operand1);
         console.log(`This is the binary value ${result}!`);
-    } else {
+    } 
+    // the rest need 2 operands
+    else {
         var operand1 = parseInt(prompt('Enter the first number:'));
         var operand2 = parseInt(prompt('Enter the second number:'));
+        console.log(`This is the binary value of operand 1: ${operand1.toString(2)}`);
+        console.log(`This is the binary value of operand 2: ${operand2.toString(2)}`);
+        switch(operation){
+            case 2:
+                console.log(`The result is ${add(operand1, operand2)} \nThe binary value is ${add(operand1,operand2).toString(2)}`);
+                break;
+
+            case 3:
+                console.log(`The result is ${sub(operand1, operand2)} \nThe binary value is ${sub(operand1,operand2).toString(2)}`);
+                break;
+
+            case 4:
+                console.log(`The result is ${mult(operand1, operand2)} \nThe binary value is ${mult(operand1,operand2).toString(2)}`);
+                break;
+
+            case 5:
+                console.log(`The result is ${div(operand1, operand2)} \nThe binary value is ${div(operand1,operand2).toString(2)}`);
+                break;
+
+            case 6:
+                console.log(`The result is ${and(operand1, operand2)} \nThe binary value is ${and(operand1,operand2).toString(2)}`);
+                break;
+
+            case 7:
+                console.log(`The result is ${or(operand1, operand2)} \nThe binary value is ${or(operand1,operand2).toString(2)}`);
+                break;
+        }
 
 
     }
