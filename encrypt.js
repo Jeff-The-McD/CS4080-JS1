0f82bb8018a94908759a2f6cd0e95410b1d461c3
Jeffrey McDonald <jffrmcdonald@gmail.com>
2021-04-05 21:00:37 -0700

    initial loop

--- encrypt.js
+++ encrypt.js
@@ -1,1 +1,38 @@
-console.log("Hello world");
+console.log("Welcome to encrpytion!");
+const prompt = require('prompt-sync')();
+
+// encrption algo 1 - AES Symmetric Encryption Algorithm
+function AES(str) {
+    console.log("In AES");
+    console.log(str.length);
+}
+
+// encrption algo 2 - DES Symmetric Encryption Algorithm
+function DES(str) {
+    console.log("In DES");
+    console.log(str.length);
+
+}
+
+// encrption algo 3 - 3DES Symmetric Encryption Algorithm
+function ThreeDES(str) {
+    console.log("In 3DES")
+    console.log(str.length);
+}
+
+done = false;
+
+// main loop
+while (!done) {
+    var plainText = prompt('Enter a string of letters:');
+    console.log(plainText);
+    AES(plainText);
+    DES(plainText);
+    ThreeDES(plainText);
+
+
+    var repeat = prompt('Are you done?(y/n):');
+    if (repeat == 'y')
+        done = true;
+
+}
