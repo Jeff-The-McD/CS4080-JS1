78414ca6f6883f2977652abd2ac6678b5d9aabb1
Deep Parekh <dparekh20@outlook.com>
2021-04-07 16:32:23 +0530

    Added caesar cipher class in encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -1,10 +1,42 @@
 console.log("Welcome to encrpytion!");
 const prompt = require('prompt-sync')();
 
-// encrption algo 1 - CEASER CIPHER
-function ceaserCipher(str) {
-    
+class CaesarCipher{
+    constructor(key){
+        this.key = key;
+        this.map = new Map();
+        var i;
+        for (i = 0; i < 26; i++) { 
+            this.map.set(i, String.fromCharCode(97+i));
+        }
+    }
 
+
+    encrypt(word){
+        word = word.toLowerCase();
+        let encrypted_word = ""; 
+        for (var i = 0; i < word.length; ++i){
+            if (word.charAt(i) === " "){
+                encrypted_word += " ";
+                continue;
+            }
+            encrypted_word += this.map.get((word.charCodeAt(i) + this.key - 97)% 26)
+        }
+        return encrypted_word;
+    }
+
+    decrypt(word){
+        word = word.toLowerCase();
+        let decrypted_word = "";
+        for (var i = 0; i < word.length; ++i){
+            if (word.charAt(i) === " "){
+                decrypted_word += " ";
+                continue;
+            }
+            decrypted_word += this.map.get((word.charCodeAt(i) - this.key - 97)% 26)
+        }
+        return decrypted_word;
+    }
 }
 
 // encrption algo 2 - RSA 
